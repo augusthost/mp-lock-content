02e2621ac24680637fdc9819576355cfd05bacf8
Ronald Aug <saisenfa@gmail.com>
2024-02-29 21:05:14 +0630

    fix: remove radius in button

--- email/approved.php
+++ email/approved.php
@@ -1,6 +1,6 @@
 Dear !!username!!,<br />
 Thank you for your interest in MPE&VCA. You now have access to news and information exclusive to our valued members. To start reading, please confirm this email address by clicking the button below.<br />
-<div style="text-align:center;"><a href="!!login_link!!" style="padding:10px 30px;border:none;background:#ffad2d;color:white; border-radius:30px; margin:10px 0 10px;">Log In</a></div><br />
+<div style="text-align:center;"><a href="!!login_link!!" style="padding:10px 30px;border:none;background:#ffad2d;color:white; margin:20px 0 20px;">Log In</a></div><br />
 I hope you enjoy our news and resources. Please reach out to info@mpevca.org with any questions or concerns.<br />
 Sincerely,
 <br />MPE&VCA
